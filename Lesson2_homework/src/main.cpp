bc91ecdf4823563b314860ef84ae5c45b5a9b635
Furiss <fur1zz28@gmail.com>
2026-08-10 20:40:49 +0300

    Fixed SOS signal

--- Lesson2_homework/src/main.cpp
+++ Lesson2_homework/src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 
 #define LED 4
-
+#define DOT_MS 200
+#define DASH_MS 600
 
 void setup() {
   Serial.begin(115200);
@@ -10,10 +11,28 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(".");
+
+
+for (int i=0; i<3; i++){
+  Serial.print(".");
   digitalWrite(LED, HIGH);
-  delay(200);
-  Serial.println("-");
+  delay(DOT_MS);
   digitalWrite(LED, LOW);
-  delay(600);
+  delay(DOT_MS);
+}
+for (int i=0; i<3; i++){
+  Serial.print("-");
+  digitalWrite(LED, HIGH);
+  delay(DASH_MS);
+  digitalWrite(LED, LOW);
+  delay(DOT_MS);//встановив інтервал між тере довжиною як крапка. Сподіваюсь так і треба було
+}
+for (int i=0; i<3; i++){
+  Serial.print(".");
+  digitalWrite(LED, HIGH);
+  delay(DOT_MS);
+  digitalWrite(LED, LOW);
+  delay(DOT_MS);
+}
+Serial.println();
 }
